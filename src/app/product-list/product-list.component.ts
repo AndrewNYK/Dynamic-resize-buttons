ffd9c9e6b16b62bbd8b62bc010c685d072c5b3b6
AndrewNYK <93440802+AndrewNYK@users.noreply.github.com>
2023-02-15 16:20:28 +0800

    1 file modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,9 +11,11 @@ export class ProductListComponent implements OnInit {
   products = products;
   max_length: number = 0;
   scale: number = 0;
+  screenWidth: number = 0;
 
   ngOnInit(): void {
-    this.scale = Math.round((10 * window.innerWidth) / window.innerHeight);
+    this.screenWidth = window.innerWidth;
+    this.calculateScale(this.screenWidth);
     for (var product of products) {
       if (product.button_description.length > this.max_length) {
         this.max_length = product.button_description.length;
@@ -26,9 +28,21 @@ export class ProductListComponent implements OnInit {
   // }
   @HostListener('window:resize', ['$event'])
   onResize(event) {
-    this.scale = Math.round(
-      (10 * event.target.innerWidth) / event.target.innerHeight
-    );
+    this.screenWidth = event.target.innerWidth;
+    this.calculateScale(this.screenWidth);
+  }
+
+  // The breakpoints for xs, sm, md, lg and xl are 576px, 768px, 992px and 1200px
+  calculateScale(width: number) {
+    if (width < 576) {
+      this.scale = 4;
+    } else if (width < 768) {
+      this.scale = 6;
+    } else if (width < 992) {
+      this.scale = 8;
+    } else if (width < 1200) {
+      this.scale = 10;
+    }
   }
 
   share() {
