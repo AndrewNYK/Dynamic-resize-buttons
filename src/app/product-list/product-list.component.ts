bcb7309b416f6da4236ca163a4066c249fffa367
AndrewNYK <93440802+AndrewNYK@users.noreply.github.com>
2023-02-15 11:15:04 +0800

    1 file modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -29,9 +29,3 @@ export class ProductListComponent implements OnInit {
     window.alert('The product has been shared!');
   }
 }
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
