420b4db53f8290a479b40be22bdd091210852806
AndrewNYK <93440802+AndrewNYK@users.noreply.github.com>
2023-02-15 11:44:55 +0800

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,10 +10,10 @@ import { products } from '../products';
 export class ProductListComponent implements OnInit {
   products = products;
   max_length: number = 0;
-  // scale: number = 0;
+  scale: number = 0;
 
   ngOnInit(): void {
-    // this.scale = window.innerWidth / window.innerHeight;
+    this.scale = Math.round((10 * window.innerWidth) / window.innerHeight);
     for (var product of products) {
       if (product.button_description.length > this.max_length) {
         this.max_length = product.button_description.length;
@@ -24,6 +24,12 @@ export class ProductListComponent implements OnInit {
   // ngOnChanges(): void {
   //   this.scale = window.innerWidth / window.innerHeight;
   // }
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.scale = Math.round(
+      (10 * event.target.innerWidth) / event.target.innerHeight
+    );
+  }
 
   share() {
     window.alert('The product has been shared!');
