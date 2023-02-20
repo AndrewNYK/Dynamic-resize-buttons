1af026998aae560167032a227fe146e6395b309e
AndrewNYK <93440802+AndrewNYK@users.noreply.github.com>
2023-02-20 16:08:41 +0800

    update: buttons resized with screen

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,14 @@
-import { Component, OnInit, OnChanges, HostListener } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  HostListener,
+  ElementRef,
+} from '@angular/core';
 
 import { products } from '../products';
+import { SCREEN_SIZE } from '../services/screen-size.enum';
+import { ResizeService } from '../services/resize.services';
 
 @Component({
   selector: 'app-product-list',
@@ -13,9 +21,48 @@ export class ProductListComponent implements OnInit {
   scale: number = 0;
   screenWidth: number = 0;
 
+  prefix = 'is-';
+  sizes = [
+    {
+      id: SCREEN_SIZE.XS,
+      name: 'xs',
+      buttonColour: 'blue',
+      scale: 4,
+      css: `d-block d-sm-none`,
+    },
+    {
+      id: SCREEN_SIZE.SM,
+      name: 'sm',
+      buttonColour: 'red',
+      scale: 6,
+      css: `d-none d-sm-block d-md-none`,
+    },
+    {
+      id: SCREEN_SIZE.MD,
+      name: 'md',
+      buttonColour: 'green',
+      scale: 8,
+      css: `d-none d-md-block d-lg-none`,
+    },
+    {
+      id: SCREEN_SIZE.LG,
+      name: 'lg',
+      buttonColour: 'yellow',
+      scale: 10,
+      css: `d-none d-lg-block d-xl-none`,
+    },
+    {
+      id: SCREEN_SIZE.XL,
+      name: 'xl',
+      buttonColour: 'green',
+      scale: 12,
+      css: `d-none d-xl-block`,
+    },
+  ];
+
   ngOnInit(): void {
-    this.screenWidth = window.innerWidth;
-    this.calculateScale(this.screenWidth);
+    //   this.screenWidth = window.innerWidth;
+    //   this.calculateScale(this.screenWidth);
     for (var product of products) {
       if (product.button_description.length > this.max_length) {
         this.max_length = product.button_description.length;
@@ -26,26 +73,68 @@ export class ProductListComponent implements OnInit {
   // ngOnChanges(): void {
   //   this.scale = window.innerWidth / window.innerHeight;
   // }
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    this.screenWidth = event.target.innerWidth;
-    this.calculateScale(this.screenWidth);
-  }
+  // @HostListener('window:resize', ['$event'])
+  // onResize(event) {
+  //   this.screenWidth = event.target.innerWidth;
+  //   this.calculateScale(this.screenWidth);
+  // }
 
-  // The breakpoints for xs, sm, md, lg and xl are 576px, 768px, 992px and 1200px
-  calculateScale(width: number) {
-    if (width < 576) {
-      this.scale = 4;
-    } else if (width < 768) {
-      this.scale = 6;
-    } else if (width < 992) {
-      this.scale = 8;
-    } else if (width < 1200) {
-      this.scale = 10;
-    }
-  }
+  // // The breakpoints for xs, sm, md, lg and xl are 576px, 768px, 992px and 1200px
+  // calculateScale(width: number) {
+  //   if (width < 576) {
+  //     this.scale = 4;
+  //   } else if (width < 768) {
+  //     this.scale = 6;
+  //   } else if (width < 992) {
+  //     this.scale = 8;
+  //   } else if (width < 1200) {
+  //     this.scale = 10;
+  //   }
+  // }
 
   share() {
     window.alert('The product has been shared!');
+  }
+
+  getButtonClass(buttonColour: string) {
+    switch (buttonColour.toLowerCase()) {
+      case 'blue':
+        return 'btn btn-primary';
+      case 'red':
+        return 'btn btn-danger';
+      case 'green':
+        return 'btn btn-success';
+      case 'yellow':
+        return 'btn btn-warning';
+      default:
+        return 'btn btn-info';
+    }
+  }
+
+  constructor(
+    private elementRef: ElementRef,
+    private resizeSvc: ResizeService
+  ) {}
+
+  @HostListener('window:resize', [])
+  private onResize() {
+    this.detectScreenSize();
+  }
+
+  ngAfterViewInit() {
+    this.detectScreenSize();
+  }
+
+  private detectScreenSize() {
+    const currentSize = this.sizes.find((x) => {
+      const el = this.elementRef.nativeElement.querySelector(
+        `.${this.prefix}${x.id}`
+      );
+      const isVisible = window.getComputedStyle(el).display != 'none';
+
+      return isVisible;
+    });
+
+    this.resizeSvc.onResize(currentSize.id);
   }
 }
